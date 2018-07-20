a3280fb13d4cf60ba530886f51ccaf4749f4a947
bensoni <bensoni@mail.gvsu.edu>
2018-07-20 16:56:15 -0400

    Added coments to can_log.py, renamed a test file
    -Added comments to "can_log.py", notably how to easily change to CAN channel if the user is on a different physical channel
    
    -Renamed "frame_logger.py" to "testing_can.py" to prevent confusion

--- Noise/can_log.py
+++ Noise/can_log.py
@@ -1,3 +1,20 @@
+"""
+Title:          CAN Logger
+Filename:       can_log.py
+Authors:        Isaac Benson & Ryan Borgeson
+Version:        1.0
+
+Description:    This python script handles monitoring the CAN bus, it is currently configured to automatically connect
+                to CAN channel 1 and update checkboxes for CAN communication status and brake status, as well as
+                updating the shifter position dial.
+
+                TO CHANGE CAN CHANNELS:
+
+                line 64:  CAN_channel = 1;
+                ^^^^^^^^^^^^^^^^^^^^^^^^^^
+                change the CAN channel on line 64 to the CAN channel you are physically connected to on the CAN box.
+"""
+
 from gui import Ui_MainWindow
 import ctypes
 
@@ -11,16 +28,18 @@ import can
 from can.io.logger import Logger
 
 class CAN(object):
+    #Initialize script, pass ui file into script to allow it to update the GUI
     def __init__(self, services):
         (self.status, self.ui) = services
 
+    #Start receive thread
     def setup(self):
         self.ui.diagnostics_textEdit.append("Setting up CAN Communication...")
         self.receiveThread = CAN_ReceiveThread(2, "CAN-Thread", self.ui)
         self.receiveThread.start()
 
 
-
+#Monitors the CAN bus, and sends wakeup frames to the shifter
 class CAN_ReceiveThread(threading.Thread):
     def __init__(self, threadID, threadName, ui):
         threading.Thread.__init__(self)
@@ -30,31 +49,39 @@ class CAN_ReceiveThread(threading.Thread):
 
     def run(self):
         try:
-            # ['critical', 'error', 'warning', 'info', 'debug', 'subdebug'][min(5, verbosity)]
+            #Set logging level to info
             logging_level_name = 'info'
             can.set_logging_level(logging_level_name)
 
+            #Set up filters, currently the logger will not filter any messages out
             can_filters = []
             config = {"can_filters": can_filters, "single_handle": True}
-            config["bustype"] = 'vector'
-            #config["bitrate"] = 500000
 
-            bus = can.interface.Bus(0, **config)
-            #print('Connected to {}: {}'.format(bus.__class__.__name__, bus.channel_info))
-            #print('Can Logger (Started on {})\n'.format(datetime.datetime.now()))
+            #Use vector box backend
+            config["bustype"] = 'vector'
+
+            #Choose a CAN channel to connect to
+            CAN_channel = 1;
+
+            #Connect to the chosen CAN channel, index is moved back by one because physical CAN channels
+            #Range from (1, 2, 3, 4, 5....N) whereas the index of the channels is (0, 1, 2, 3, 4...N-1)
+            bus = can.interface.Bus(CAN_channel - 1, **config)
 
             prev_position = 0
             wakeup_msg = can.Message(arbitration_id=0x59e, data=[0, 0, 0, 0, 0, 0, 0, 0], extended_id=False)
-            #self.ui.brakeEngaged_checkBox.setEnabled(False)
-            #self.ui.canCom_checkBox.setEnabled(False)
 
             try:
+                #Send wakeup message
                 bus.send(wakeup_msg)
 
                 while True:
+                    #Wait for a CAN message
                     msg = bus.recv(1)
 
+                    #If a valid message was received
                     if msg is not None:
+
+                        #If message received was Gear_Shift_by_Wire, obtain the current position and update dial
                         if (msg.arbitration_id == 0x5a):
                             position = (msg.data[3] & 0xf8) >> 3
                             bus.send(wakeup_msg)
@@ -72,9 +99,7 @@ class CAN_ReceiveThread(threading.Thread):
                             prev_position = position
                             self.ui.canCom_checkBox.setChecked(True)
 
-
-
-
+                        #If message received was Body_Info_4_HS2, obtain brake status and update brake checkbox
                         if (msg.arbitration_id == 0x242):
                             brake = (msg.data[2] & 0x80) >> 7
                             if (brake == 0x01):
@@ -85,7 +110,7 @@ class CAN_ReceiveThread(threading.Thread):
 
 
 
-
+            #Handle keyboard interrupts (mainly for proper exit upon Ctrl + C shutdown)
             except KeyboardInterrupt:
                 pass
             finally:
